9ca581bf05bc91055144b0ce67a016902f5093bd
nerdstarcode <sthivendev@gmail.com>
2022-01-08 04:59:33 -0300

    adiçao de graficos com o chart.js

---
+++ js/charts.js
@@ -0,0 +1,52 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'polarArea',
+    data: {
+        labels: ['Facebook', 'YouTube', 'Amazon'],
+        datasets: [{
+            label: 'Traffic Source',
+            data: [1200, 1900, 3000],
+            backgroundColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true, 
+    }
+});
+var earning = document.getElementById('earning').getContext('2d');
+var myChart = new Chart(earning, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [{
+            label: 'Earning',
+            data: [1200, 1900, 3000, 5400, 1220, 3600, 4800, 4578, 2566, 8545, 4587, 2567],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
